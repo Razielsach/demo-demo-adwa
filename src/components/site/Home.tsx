166c47ecf9216a708cfb3f9cacf81d887fb1dde9
Razielsach <Devilonyourshooulder@proton.me>
2026-06-09 20:33:17 +0300

    Initial commit

---
+++ src/components/site/Home.tsx
@@ -0,0 +1,32 @@
+import { Nav } from "./Nav";
+import { Hero } from "./Hero";
+import { ScrollMorphHero } from "./ScrollMorphHero";
+import { Mission } from "./Mission";
+import { Services } from "./Services";
+import { WhyUs } from "./WhyUs";
+import { Testimonials } from "./Testimonials";
+import { Insights } from "./Insights";
+import { FAQ } from "./FAQ";
+import { CTA } from "./CTA";
+import { Footer } from "./Footer";
+import { Reveal } from "./Reveal";
+
+export function Home() {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <Nav />
+      <main>
+        <Hero />
+        <Reveal><ScrollMorphHero /></Reveal>
+        <Reveal><Mission /></Reveal>
+        <Reveal><Services /></Reveal>
+        <Reveal><WhyUs /></Reveal>
+        <Reveal><Testimonials /></Reveal>
+        <Reveal><Insights /></Reveal>
+        <Reveal><FAQ /></Reveal>
+        <Reveal><CTA /></Reveal>
+      </main>
+      <Footer />
+    </div>
+  );
+}
